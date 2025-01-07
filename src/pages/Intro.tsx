3184da43b7c2d081c139949ba3e509625c1cd0a2
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-01-07 13:46:52 +0100

    feat: add font Poppins

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom'
-
 const Intro = () => {
   return (
     <div className="intro-page relative">
@@ -10,7 +9,9 @@ const Intro = () => {
         <h1 className="text-2xl font-bold pl-6 pr-18 pb-6">
           Enjoy Your Movie <br /> Watch Everywhere
         </h1>
-        <h2 className="text-lg pb-8">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</h2>
+        <h2 className="text-lg pb-8 font-poppins font-normal">
+          Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.
+        </h2>
         <Link to={'/home'}>
           <button className="bg-red-500 text-white hover:bg-slate-600 rounded-md px-8 py-2 text-ml">Get Started</button>
         </Link>
