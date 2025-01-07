99997846e41525bffb01eaead2da73f0f53d01a3
Mark1U <mark@zurcloud.com>
2025-01-07 15:54:24 +0100

    Fix: merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import './App.css'
-import RootLayout from './RootLayout/RootLayout'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import Intro from './pages/Intro'
-import Splashscreen from './pages/Splashscreen'
-import Genre from './pages/Genre'
-import MovieDetails from './pages/MovieDetails'
-import { AnimatePresence } from 'framer-motion'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import RootLayout from "./RootLayout/RootLayout";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Intro from "./pages/Intro";
+import Splashscreen from "./pages/Splashscreen";
+import MovieDetails from "./pages/MovieDetails";
+import { AnimatePresence } from "framer-motion";
+import MovieLists from "./pages/MovieLists";
 
 function App() {
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
@@ -18,9 +22,9 @@ function App() {
           <Route index element={<Splashscreen />} />
           <Route path="intro" element={<Intro />} />
           <Route path="home" element={<Home />} />
-          <Route path="popular" element={<Genre />} />
-          <Route path="search/:query" element={<Genre />} />
-          <Route path="genre/:query" element={<Genre />} />
+          <Route path="popular" element={<MovieLists />} />
+          <Route path="search/:query" element={<MovieLists />} />
+          <Route path="genre/:query" element={<MovieLists />} />
           <Route path="favorites" element={<Favorites />} />
           <Route path="movie/:movieId/:title" element={<MovieDetails />} />
         </Route>
@@ -34,7 +38,7 @@ function App() {
         <RouterProvider router={router} />
       </AnimatePresence>
     </div>
-  )
+  );
 }
 
 export default App;
