69e2f9088205be985e585854b74cb5c1283fc5ac
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-01-08 12:39:37 +0100

    fix: corect breite

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import "./App.css";
-import RootLayout from "./RootLayout/RootLayout";
-import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
-import Intro from "./pages/Intro";
-import Splashscreen from "./pages/Splashscreen";
-import MovieDetails from "./pages/MovieDetails";
-import { AnimatePresence } from "framer-motion";
-import MovieLists from "./pages/MovieLists";
-import { FavoritesContext } from "./contexts/FavoritesContext";
-import { useState } from "react";
-import { Movie } from "./interfaces/Movie";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import RootLayout from './RootLayout/RootLayout'
+import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import Intro from './pages/Intro'
+import Splashscreen from './pages/Splashscreen'
+import MovieDetails from './pages/MovieDetails'
+import { AnimatePresence } from 'framer-motion'
+import MovieLists from './pages/MovieLists'
+import { FavoritesContext } from './contexts/FavoritesContext'
+import { useState } from 'react'
+import { Movie } from './interfaces/Movie'
 
 function App() {
-  const [favorites, setFavorites] = useState<Movie[]>([]);
+  const [favorites, setFavorites] = useState<Movie[]>([])
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -33,21 +28,19 @@ function App() {
           <Route path="favorites" element={<Favorites />} />
           <Route path="movie/:movieId/:title" element={<MovieDetails />} />
         </Route>
-      </>
-    )
-  );
+      </>,
+    ),
+  )
 
   return (
-    <div className="flex items-center justify-center flex-col w-[428px] h-screen">
+    <div className="flex items-center justify-center flex-col w-[428px] m-auto">
       <AnimatePresence>
         <FavoritesContext.Provider value={{ favorites, setFavorites }}>
-
           <RouterProvider router={router} />
-
         </FavoritesContext.Provider>
       </AnimatePresence>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Splashscreen.tsx
+++ src/pages/Splashscreen.tsx
@@ -28,8 +28,8 @@ const Splashscreen = () => {
         duration: 1,
         ease: 'backInOut',
       }}
-      className="bg-[#FC2121] rounded-[50px] w-[428px] h-[926px] flex flex-col justify-center items-center">
-      <h1 className="text-[44px] text-white">.MOV</h1>
+      className="bg-[#FC2121] w-[428px] h-[926px] flex flex-col justify-center items-center">
+      <h1 className="text-white text-center font-montserrat text-[44px] font-extrabold leading-none">.MOV</h1>
     </motion.section>
   )
 }
