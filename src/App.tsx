8001062badf1f548aeba44edec1d55409bf656a3
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-01-08 10:38:31 +0100

    feat: add carousel with swiper libary

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   )
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center flex-col w-[428px] h-screen">
       <AnimatePresence>
         <RouterProvider router={router} />
       </AnimatePresence>
