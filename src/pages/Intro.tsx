086ec62bc70838bffa24dd7ca5d5002d704eb5de
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-01-09 10:45:42 +0100

    feat: add animation on intro seite

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,11 +1,51 @@
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 const Intro = () => {
   return (
-    <div className="intro-page relative">
-      <div className="pt-10 bg-custom-gradient">
-        <img src="./IMGs/intro-view-img.png" alt="Preview image" />
+    <div className="intro-page relative ">
+      <div className="pt-10 bg-custom-gradient h-[700px] relative overflow-hidden ">
+        {/* <img src="./IMGs/intro-view-img.png" alt="Preview image" /> */}
+        <motion.img
+          initial={{
+            opacity: 0,
+            x: 200,
+            rotate: 20,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            rotate: 0,
+          }}
+          transition={{
+            duration: 1,
+            ease: 'easeInOut',
+          }}
+          className="absolute z-30 right-2"
+          src="./IMGs/Description.png"
+          alt="Preview image"
+        />
+
+        <motion.img
+          initial={{
+            opacity: 0,
+            x: -200,
+            rotate: -20,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            rotate: 0,
+          }}
+          transition={{
+            duration: 1,
+            ease: 'easeInOut',
+          }}
+          className="absolute z-20 left-2"
+          src="./IMGs/Home.png"
+          alt="Preview image"
+        />
       </div>
-      <div className="text-center p-12 absolute top-2/3 left-0 w-full z-10 mt-5 bg-white">
+      <div className="text-center p-12 absolute top-2/3 left-0 w-full z-10 mt-5 bg-white z-50">
         <h1 className="text-2xl font-bold pl-6 pr-18 pb-6">
           Enjoy Your Movie <br /> Watch Everywhere
         </h1>
